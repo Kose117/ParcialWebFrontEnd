757cec8f31e9fa06799c525cd9e17263578a59db
JoseRodriguez <93455373+Kose117@users.noreply.github.com>
2024-05-21 20:27:07 -0500

    Delete ParcialWebFrontEnd directory

--- ParcialWebFrontEnd/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListarLibrosComponent } from './components/listar-libros/listar-libros.component';
-
-export const APP_ROUTES: Routes = [
-  { path: 'listar-libros', component: ListarLibrosComponent },
-  { path: '', redirectTo: '/listar-libros', pathMatch: 'full' }
-];
